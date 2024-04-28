3a862586911958da8ce94ec962dc8aabc3234da9
tamo-tmry <mollianium@gmail.com>
2024-04-28 15:44:39 +0900

    chore: test

--- src/app/components/atoms/CheckboxItem.tsx
+++ src/app/components/atoms/CheckboxItem.tsx
@@ -8,6 +8,7 @@ type CheckboxItemProps = {
 export const CheckboxItem = ({itemId, label, isChecked, onChangeFn}: CheckboxItemProps) => {
     return (
         <>
+            oo
             <input
                 type='checkbox'
                 id={itemId}
