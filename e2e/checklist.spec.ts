251e11fc27ebdbe20c9a72a0e5c12166bfc73aa8
tamo-tmry <mollianium@gmail.com>
2024-05-03 23:51:23 +0900

    feat: E2Eテストの追加

---
+++ e2e/checklist.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+const appUrl = process.env.NEXT_PUBLIC_BASE_URL!
+
+test('チェックリストが表示されること', async ({ page }) => {
+    await page.goto(`${appUrl}/checklist`);
+    const checkbox = await page.getByRole('checkbox').all();
+    expect(checkbox.length).toBeGreaterThan(0);
+});
