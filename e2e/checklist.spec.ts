ae6ef61eeb9d368da71371f631fb3d0e0b60a363
tamo-tmry <mollianium@gmail.com>
2024-05-04 00:24:08 +0900

    fix: 環境変数の参照修正

--- e2e/checklist.spec.ts
+++ e2e/checklist.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-const appUrl = process.env.BASE_URL!
-
 test('チェックリストが表示されること', async ({ page }) => {
-    await page.goto(`${appUrl}/checklist`);
+    await page.goto(`http://localhost:3000/checklist`);
     const checkbox = await page.getByRole('checkbox').all();
     expect(checkbox.length).toBeGreaterThan(0);
 });
