3df1e6053b85ae72930e2b7a4e5426755c485c4f
tamo-tmry <mollianium@gmail.com>
2024-05-19 20:51:11 +0900

    ci: test

--- e2e/checklist.spec.ts
+++ e2e/checklist.spec.ts
@@ -4,8 +4,6 @@ const appUrl = process.env.NEXT_PUBLIC_BASE_URL!;
 
 test('チェックリストが表示されること', async ({ page }) => {
     await page.goto(`${appUrl}/checklist`);
-    const title = await page.locator('h1').innerText();
-    console.log(title)
     const checkbox = await page.getByRole('checkbox').all();
     expect(checkbox.length).toBeGreaterThan(0);
 });
