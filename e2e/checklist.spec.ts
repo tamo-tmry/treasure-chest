20ab219a312929097e0f8046cd017c2520f6661b
tamo-tmry <mollianium@gmail.com>
2024-05-19 17:35:55 +0900

    ci: test

--- e2e/checklist.spec.ts
+++ e2e/checklist.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 const appUrl = process.env.NEXT_PUBLIC_BASE_URL!;
 
 test('チェックリストが表示されること', async ({ page }) => {
+    console.log('DEBUG URL: ',`${appUrl}/checklist`);
     await page.goto(`${appUrl}/checklist`);
     const checkbox = await page.getByRole('checkbox').all();
     expect(checkbox.length).toBeGreaterThan(0);
